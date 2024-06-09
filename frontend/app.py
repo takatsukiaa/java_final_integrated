0e0b6ca8237402f2f84e77fb5042308d027ed132
owenowenisme <mses010108@gmail.com>
2024-06-09 09:54:13 +0000

    feat: add copy on result and renew regex

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/<path:url_hash>')
+def redirect_anystring(url_hash):
+    return redirect(f'/api/{url_hash}')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=3000)
